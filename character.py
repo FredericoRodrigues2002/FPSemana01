3b434e20f4b76d0a1339e257747e9488e7db77fd
Fred <fredericorodrigues.work@gmail.com>
2025-11-07 18:40:00 +0000

    Third Commit

--- character.py
+++ character.py
@@ -11,12 +11,19 @@ third_name = input("Nome:")
 third_attack = int(input("Ataque:"))
 third_defence = int(input("Defesa:"))
 
-array_bidimensional = [[str(first_name), str(first_attack), str(first_defence)], [str(second_name), str(second_attack), str(second_defence)], [str(third_name), str(third_attack), str(third_defence)]]
+array_bidimensional = [[first_name, first_attack, first_defence], [second_name, second_attack, second_defence], [third_name, third_attack, third_defence]]
 
 print(array_bidimensional)
 
-a = sorted(array_bidimensional)
+strongest_attack = array_bidimensional[0]
+strongest_defence = array_bidimensional[0]
 
-print("Ataque", max(a[0]))
-print("Defesa", max(a[0]))
 
+for x in array_bidimensional:
+    if x[1] > strongest_attack[1]:
+        strongest_attack = x
+    if x[2] > strongest_defence[2]:
+        strongest_defence = x
+
+print("Ataque", strongest_attack[0], strongest_attack[1])
+print("Defesa", strongest_defence[0], strongest_defence[2])
